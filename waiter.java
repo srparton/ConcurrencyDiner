0de4b4d752ff646d1cd3e14f335eac093a5c1533
Jason Shick <jlshick1@buffs.wtamu.edu>
2022-04-11 17:12:16 -0500

    Saving to study for Android

--- waiter.java
+++ waiter.java
@@ -2,7 +2,40 @@ import java.util.BitSet;
 import java.util.concurrent.*;
 
 public class Waiter {
-    private  BitSet dinnersEating;
-    public BlockingQueue<Int> queue;
+    private BitSet dinersEating;
+    private int numOfDiners;
+    private int doneEating = 0;
+    private TransferQueue<Integer> queue;
 
+    public Waiter (int numOfDiners){
+        dinersEating = new BitSet(numOfDiners);
+    }
+
+    public Waiter() {
+    }
+
+    public void setNumOfDiners (int numOfDiners){
+        dinersEating = new BitSet(numOfDiners);
+    }
+
+    public void startService() {
+        Integer dinerNum;
+        while (doneEating < numOfDiners){
+            try {
+                dinerNum = queue.take();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                System.out.println("Diner service interupted!");
+                return;
+            }
+            if(!dinersEating.get(dinerNum - 1) && !dinersEating.get(dinerNum + 1)){
+                dinersEating.set(dinerNum);
+            }
+        }
+    }
+
+    public void page(int dinerNum) {
+        
+    }
 }
