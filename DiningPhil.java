44d32d361e0e8740a1b65aeaf2d07395dbff3528
Jacob Sisk <jmsisk1@buffs.wtamu.edu>
2022-04-11 14:33:32 -0500

    Run() no longer has any errors or problems; still needs to be connected to main

--- DiningPhil.java
+++ DiningPhil.java
@@ -1,9 +1,11 @@
 import java.util.concurrent.Semaphore;
+import java.lang.Thread;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DiningPhil extends Thread {
     
     public int thread_id; // Used in constructor for DiningPhil
-    private static final int MAX_TIME = 10000; // May not be needed in java
+    //private static final int MAX_TIME = 10000; // May not be needed in java
     private static final int EAT = 2; //How many times should each philo eat
     private static final int DINERS_NUM = 5; //Number of philos
     private static Semaphore[] forks = new Semaphore[DINERS_NUM];// Array of semaphrores for forks.
@@ -12,13 +14,49 @@ public class DiningPhil extends Thread {
         thread_id = id;
     }
 
-    public static void diner() {
-        // IMPLEMENT DINER CODE HERE. 
+    public static void diner(int id) {
+
     }
 
-    public void run() {
-        // INSERT THREAD CODE HERE. 
+    public void run(int id) throws InterruptedException {
+        System.out.println("Diner " + id + " is sitting down at the table.");
+        int timesEaten = 0;
+        Semaphore firstFork;
+        Semaphore secondFork;
+        if(id % 2 == 0){                        
+            firstFork = forks[id];      //Half of the diners will call for their left
+            secondFork = forks[(id+1) % DINERS_NUM];   //fork first, then call for their right fork.
+        }
+        else{
+            firstFork = forks[(id+1) % DINERS_NUM];    //The other half of the diners will call for
+            secondFork = forks[id];     //the right fork then the left fork.
+        }
+        while (timesEaten < EAT){
+            int sleepTime = ThreadLocalRandom.current().nextInt(0,2000); //determine a random amount of time between 0 and 2 seconds
+            System.out.println("Diner " + id + " says they are thinking.");
+            Thread.sleep(sleepTime); //Diner "Thinks"
+            System.out.println("Diner " + id + " says they are hungry.");
+
+            //Code for Grabbing Forks
+            firstFork.acquire();
+            secondFork.acquire(); 
+
+
+            System.out.println("Diner " + id + " has forks and is eating.");
+            timesEaten++;
+
+            sleepTime = ThreadLocalRandom.current().nextInt(0,2000);
+            Thread.sleep(sleepTime); //Diner "eats" for rand time between 0 and 2 seconds
+
+            System.out.println("Diner " + id + " is content. Putting forks back.");
+
+            firstFork.release();
+            secondFork.release();
+
+        }
+        System.out.println("Diner " + id + " is full, they are leaving the table.");
     }
+
     public static void main(String[] args) {
         // USE IF WE WANT USER INPUT FOR DINER_NUM
         // do {
