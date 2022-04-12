68f0969ed1fb21cc08cce174143a92ada6626059
Jason Shick <viperb5@gmail.com>
2022-04-11 21:13:57 -0500

    Save

--- waiter.java
+++ waiter.java
@@ -18,24 +18,47 @@ public class Waiter {
         dinersEating = new BitSet(numOfDiners);
     }
 
-    public void startService() {
+    public void startService(DiningPhil[] diners) {
         Integer dinerNum;
         while (doneEating < numOfDiners){
             try {
                 dinerNum = queue.take();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
+            } catch (InterruptedException e) {               
                 e.printStackTrace();
                 System.out.println("Diner service interupted!");
                 return;
             }
-            if(!dinersEating.get(dinerNum - 1) && !dinersEating.get(dinerNum + 1)){
+            if (dinerNum == 0){
+                if (!dinersEating.get(numOfDiners - 1) && !dinersEating.get(1)){
+                    dinersEating.set(0);
+                    diners[0].interrupt();
+                }
+            }
+            else if (!dinersEating.get(dinerNum - 1) && !dinersEating.get((dinerNum + 1) % numOfDiners)){
                 dinersEating.set(dinerNum);
+                diners[dinerNum].interrupt();
+            }
+            else { 
+                try {
+                queue.add(dinerNum);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Out of room in the queue, this shouldn't happen.");
+                    System.exit(1);
+                }
+
             }
         }
     }
 
     public void page(int dinerNum) {
-        
+        try {
+            queue.put(dinerNum);
+        } catch (InterruptedException e) {
+            System.out.println("Out of room in the queue, this shouldn't happen.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        Thread.sleep();
     }
 }
